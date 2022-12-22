38bc94eec53096f39c65f7cda7196242f20e673c
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2022-12-22 23:39:05 +0530

    Create name with len.py

---
+++ name with len.py
@@ -0,0 +1,9 @@
+#This code prints the no. of characters in a user's name
+
+print( len( input("What is your name?") ) )
+
+
+#Notes
+#If input was "Zunairah"
+#Ist: print(len("Zunairah"))
+#2nd: print(8)
