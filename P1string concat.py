0c45e083e0a3067a7217ef37b93ecd3556c9f42d
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2022-12-23 23:07:41 +0530

    Create P1string concat.py

---
+++ P1string concat.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print("welcome to the band name generator")
+
+#2. Ask the user for the city that they grew up in.
+city = input("which city did you grow up in?\n") 
+#3. Ask the user for the name of a pet.
+pet = input("what is the name of your pet\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("your band name could be "  + city +  " "  + pet)
