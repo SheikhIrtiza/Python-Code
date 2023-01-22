769e8af6b6f67b107c3df627a7b3adcb5c38f46a
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-01-22 18:03:34 +0530

    Update Hangman4.py

--- Hangman4.py
+++ Hangman4.py
@@ -1,6 +1,5 @@
 import random
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
 from hangman_words import word_list
 
@@ -10,7 +9,6 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo
 print(logo)
 
@@ -25,7 +23,6 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
         print(f"You've already guessed {guess}")
 
@@ -38,7 +35,6 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         
         lives -= 1
@@ -54,6 +50,5 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
     print(stages[lives])
