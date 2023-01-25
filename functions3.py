feaadf1546f6f72beb12946a005a8bf6ea45bc9f
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-01-25 23:42:01 +0530

    Create functions3.py

---
+++ functions3.py
@@ -0,0 +1,32 @@
+#Simple function
+#def message():
+ # print("Hello Irtiza!")
+ # print("how do you do Irtiza")
+ # print("Isn't the weather so cold today")
+
+#message()
+
+#Function that allows for input
+
+#def message_with_name(name):
+ # print(f"Hello {name}")
+ #  print(f"How do you do {name}?")
+
+#message_with_name("Irtiza")
+
+#OR
+
+def text_with_name(name):
+  print(f"Asalamualaikum {name}")
+  print(f"How are you dear {name}?")
+
+text_with_name("Soliha")
+
+# Functions with ,more than 1 input
+def greet_with(name, location):
+  print(f"Hii {name}")
+  print(f"what is it like in {location}")
+
+#greet_with("Sheikh Soliha", "Nowhere")
+
+greet_with(name="Soliha", location="Pulwama")
