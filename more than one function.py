f10167b48f31aaf3c6897c15027e3d208e10f316
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-01-25 23:40:30 +0530

    Create more than one function.py

---
+++ more than one function.py
@@ -0,0 +1,24 @@
+#Simple function
+#def message():
+ # print("Hello Irtiza!")
+ # print("how do you do Irtiza")
+ # print("Isn't the weather so cold today")
+
+#message()
+
+#Function that allows for input
+
+#def message_with_name(name):
+#  print(f"Hello {name}")
+# print(f"How do you do {name}?")
+
+#message_with_name("Irtiza")
+
+# Functions with ,more than 1 input
+def greet_with(name, location):
+  print(f"Hii {name}")
+  print(f"what is it like in {location}")
+
+#greet_with("Sheikh Soliha", "Nowhere")
+
+greet_with(name="Soliha", location="Pulwama")
