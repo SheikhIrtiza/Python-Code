39e8d6a0522fd6a81ac5a5ee9498695b00809d47
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-01-27 23:04:18 +0530

    Create Simple calculator.py

---
+++ Simple calculator.py
@@ -0,0 +1,40 @@
+#Calculator
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+
+#Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+
+    #Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+
+    #Divide
+def divide(n1, n2):
+    return n1 / n2 #quotient
+
+    #Modulus Division
+
+
+def mod(n1, n2):
+    return n1 % n2 #remainder
+
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide, "%": mod}
+
+num1 = int(input("What's the first number?:"))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above:")
+num2 = int(input("What's the second number?:"))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
