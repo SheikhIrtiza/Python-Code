f5b95a16fe040d4efe9a78930f878ab16facf52d
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-02-12 23:56:05 +0530

    Create Coffee Machine Req.py

---
+++ Coffee Machine Req.py
@@ -0,0 +1,50 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def is_resource_sufficient(order_ingredients):
+    is_on = True
+
+
+while True:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']} ml")
+        print(f"Coffee: {resources['coffee']} g")
+        print("Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        is_resource_sufficient(drink["ingredients"])
