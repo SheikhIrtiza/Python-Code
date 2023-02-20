95d4c573edb1e95ff31d2579a7361ed9a9ee4c9a
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-02-20 23:37:22 +0530

    Update Object and Class.py
    add another module.py with this file

--- Object and Class.py
+++ Object and Class.py
@@ -16,3 +16,4 @@ my_screen.exitonclick()#my_screen = fxn, fxn is tied to objects so its actually 
 #method name is exitonclick and this will allow our program to continue running untill we click on the screen and then it exit our code
 
 #we can create a new Object from a blueprint, we can tap into its attributes by using the object name dot then the name of the attribute
+ 
