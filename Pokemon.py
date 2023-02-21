36c506670a1bd3323b3ea413278492bc2fd0250a
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-02-21 22:52:29 +0530

    Update Pokemon.py

--- Pokemon.py
+++ Pokemon.py
@@ -1,8 +1,9 @@
-from prettytable import PrettyTable 
+from prettytable import PrettyTable #install the package first
 table = PrettyTable()
 #Adding methods
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
+table.add_column("type2", ["a", "b", "c"])#we can add more no. of columns 
 
 #Adding attributes
 table.align = "l"
