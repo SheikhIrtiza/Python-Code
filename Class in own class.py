85b24673e8bb360df152e299686fb10e9eab4ce5
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-02-23 23:14:28 +0530

    Create Class in own class.py

---
+++ Class in own class.py
@@ -0,0 +1,18 @@
+class User:
+    pass
+
+
+user_1 = User()
+
+#Now to create an attributr for that class we can write the code as
+user_1.id = "001"
+user_1.username = "Irtiza"
+
+print(user_1.username)#This will print the username
+print(user_1.id)#this will print the user_id
+
+user_2 = User()
+user_2.id = "002"
+user_2.name = "Ziya"
+
+print(user_2.name)
