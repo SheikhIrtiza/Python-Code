021cd45299b789706b08abcf485190abf8434023
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-03-02 23:44:08 +0530

    Add files via upload

---
+++ Question Bank/question_model.py
@@ -0,0 +1,9 @@
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+new_q = Question("What is your name", "False")
+print(new_q.text)
