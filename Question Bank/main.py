e4b8021329e138ef2da2fed0bee74e34458e164d
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-03-03 23:41:50 +0530

    Update main.py

--- Question Bank/main.py
+++ Question Bank/main.py
@@ -17,7 +17,7 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
-    quiz.next_question()
+    quiz.next_question()         
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
