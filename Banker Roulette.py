ac751cec903e96980dcf6d8452e01a0376cd5ee4
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-03-05 21:38:45 +0530

    Update Banker Roulette.py

--- Banker Roulette.py
+++ Banker Roulette.py
@@ -14,4 +14,4 @@ random_choice = random.randint(0, num_items - 1)
 #Pick out random person from list of names using the random number.
 person_who_will_pay = names[random_choice]
 
-print(person_who_will_pay + " is going to buy the meal today!")
+print(person_who_will_pay + " is going to buy the meal today!")  
