a96be74628b0b017adbfe23081e555c0f4b05b2d
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-03-07 16:20:02 +0530

    Add files via upload

---
+++ diff.shapes of Turtle-Graphics/main.py
@@ -0,0 +1,19 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat" "SlateGrey", "SeaGreen"]
+#for more attractive colors we can go through with this "linkhttps://trinket.io/"
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
+
+
