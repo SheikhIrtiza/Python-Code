88d70b9fb3f6504f20328d1402e703a2ea11d0fa
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-03-19 23:34:49 +0530

    Update BodyMassIndex(BMI).py

--- BodyMassIndex(BMI).py
+++ BodyMassIndex(BMI).py
@@ -6,7 +6,7 @@ weight = float(input("enter your weight in kg: "))
 bmi = round(weight / height ** 2)
 if bmi < 18.5:
     print(f"Your bmi is {bmi}, you are underweight.")
-elif bmi < 25:
+elif bmi < 25: 
     print(f"Your bmi is {bmi}, you are normal.")
 elif bmi < 30:
     print(f"Your bmi is {bmi}, you are slightly overweight.")
