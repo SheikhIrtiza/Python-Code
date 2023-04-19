4ee48bff80813ba1f7d410f238597a08d9730e45
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-04-19 20:50:27 +0530

    Update BodyMassIndex(BMI).py

--- BodyMassIndex(BMI).py
+++ BodyMassIndex(BMI).py
@@ -13,7 +13,7 @@ elif bmi < 30:
 elif bmi < 35: 
     print(f"Your bmi is {bmi}, you are obese.") 
 else: 
-    print(f"Your bmi is {bmi}, you are clinically obese.")   
+    print(f"Your bmi is {bmi}, you are clinically obese.")    
  
  
  
