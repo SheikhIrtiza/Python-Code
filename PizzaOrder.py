e65d15509a784db7c3fc6a26057f19302d52f625
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-05-09 09:30:54 +0530

    Update PizzaOrder.py

--- PizzaOrder.py
+++ PizzaOrder.py
@@ -1,7 +1,7 @@
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")    
 
 
 bill = 0
