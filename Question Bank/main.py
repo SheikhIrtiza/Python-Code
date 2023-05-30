c61a07dee3a7f3b6288641cdf10f65ea0324c5c8
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-05-30 23:56:04 +0530

    Update main.py

--- Question Bank/main.py
+++ Question Bank/main.py
@@ -6,7 +6,7 @@ question_bank = []
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
+    new_question = Question(question_text, question_answer) 
     question_bank.append(new_question)
 
 #print(question_bank[0].answer)
