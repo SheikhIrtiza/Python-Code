5431e8c9e4c57ec613ebcbeba4a2e5b1a6ceba0d
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-06-03 22:55:42 +0530

    Update cipher_text2.py

--- cipher_text2.py
+++ cipher_text2.py
@@ -8,7 +8,7 @@ def encrypt(plain_text, shift_amount):
   cipher_text = ""
   for letter in plain_text:
     position = alphabet.index(letter)
-    new_position = position + shift_amount
+    new_position = position + shift_amount 
     cipher_text += alphabet[new_position]
   print(f"The encoded text is {cipher_text}")
 
