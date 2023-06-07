1822ef622f1bb85b5ff9b3d41ee31a8ec8cc63a7
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-06-07 23:49:40 +0530

    Create palindrome-string.py

---
+++ palindrome-string.py
@@ -0,0 +1,17 @@
+def is_palindrome(string):
+    # Remove spaces and convert to lowercase
+    string = string.replace(" ", "").lower()
+    # Reverse the string
+    reversed_string = string[::-1]
+    # Check if the original and reversed strings are equal
+    if string == reversed_string:
+        return True
+    else:
+        return False
+
+# Example usage
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
