8ed20db55a59dbccdda87f584c59a192f43b8f50
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-06-07 23:55:17 +0530

    Update Hangman3.py

--- Hangman3.py
+++ Hangman3.py
@@ -3,7 +3,7 @@ import random
 
 from hangman_words import word_list
 
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(word_list) 
 word_length = len(chosen_word)
 
 end_of_game = False
