61ced681b607478ddc525c8411109910b689a6ea
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-06-14 23:14:11 +0530

    Update Scope and Number.py

--- Scope and Number.py
+++ Scope and Number.py
@@ -4,7 +4,7 @@
 
 #def increase_enemies():
  # enemies = 2
-  #print(f"enemies inside function: {enemies}")
+  #print(f"enemies inside function: {enemies}") 
 
 #increase_enemies()
 #print(f"enemies outside function: {enemies}")
