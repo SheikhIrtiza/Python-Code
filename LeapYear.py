201162e7da599674c9bfce24db7f22fac627f0fa
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-06-19 23:18:15 +0530

    Update LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -5,7 +5,7 @@ if year % 4 == 0:
     if year % 400 == 0:
         print("Leap year")
     else:
-        print("Not leap year")
+        print("Not leap year") 
   else:
         print("Leap year")
 else:
