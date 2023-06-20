d66a5fd451876c33a14dea1a5f4e0eb11dd1252c
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-06-20 23:17:47 +0530

    Update life in weeks.py

--- life in weeks.py
+++ life in weeks.py
@@ -6,7 +6,7 @@ age_as_int = int(age)
 years_remaining = 90 - age_as_int
 
 days_remaining = years_remaining * 365
-weeks_remaining = years_remaining * 52
+weeks_remaining = years_remaining * 52 
 months_remaining = years_remaining * 12
 
 # print(months_remaining)
