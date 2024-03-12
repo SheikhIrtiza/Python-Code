2cc2969d432efbdd7d362696cf0c8d796dffd2f4
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2024-03-12 23:08:27 +0530

    Create time.py

---
+++ time.py
@@ -0,0 +1,23 @@
+# import time
+
+# start = time.time()
+# for i in range(1, 101):
+#     print(i)
+    
+# print(time.time() - start)
+# another implementation
+
+import time
+start = time.time()
+i = 1
+while i<101:
+    print(i)
+    i+=1
+    
+print(time.time() - start)
+# problems with this approach
+# different time for different algorithm  
+# time varies if implementation changes
+# different machines different time
+# does not work for extremely small input
+# time varies for different inputs, but can't establish a relationship
