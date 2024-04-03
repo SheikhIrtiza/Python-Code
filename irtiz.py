5c6587de8949ea10d9a0a7d066a04f67f84d4a39
Sheikh Irtiza <sheikhirtiza444@gmail.com>
2024-04-03 23:13:23 +0530

    Update irtiz.py

--- irtiz.py
+++ irtiz.py
@@ -1,1 +1,10 @@
+name = "Sheikh Irtiza"
+job = "Junior AI Developer"
+age = 24
+address = "Pulwama, Jammu and Kashmir"
 
+# Printing the information
+print("Name:", name)
+print("Job:", job)
+print("Age:", age, "years old")
+print("Address:", address)
